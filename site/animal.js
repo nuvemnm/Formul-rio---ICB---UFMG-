16f203c2ae35df351de31cc84e080bce1f278648
Matheus Gregor <159480088+nuvemnm@users.noreply.github.com>
2024-04-08 08:38:17 -0300

    Add files via upload

--- site/animal.js
+++ site/animal.js
@@ -1,18 +1,29 @@
 
 
-const btnSubmit = document.querySelector("#pdf");
+const cpm1quantidade = document.querySelector('#quantidade');
+const cpm1idade = document.querySelector('#idade');
+const cpm1peso = document.querySelector('#peso');
+const cpm1especie = document.querySelector('#especie');
 
-btnSubmit.addEventListener("click", (event) => {
-    event.preventDefault(); // Previne o comportamento padrão do botão de enviar
-    window.print(); // Abre a janela de impressão
-});
 
-const form = document.querySelector("#form")
-const linhagem = document.querySelector("#linhagem")
-const quantidade = document.querySelector("#quantidade")
-const idade = document.querySelector("#idade")
-const peso = document.querySelector("#peso")
-const requerente = document.querySelector("#requerente")
-const orientador = document.querySelector("#orientador")
-const ramal = document.querySelector("#ramal")
-const unidade = document.querySelector("#unidade")
+
+const cpm2quantidade = document.querySelector('#quantidade2');
+const cpm2idade = document.querySelector('#idade2');
+const cpm2peso = document.querySelector('#peso2');
+const cpm2especie = document.querySelector('#especie')
+
+function atualizacpm2(){
+    cpm2quantidade.value = cpm1quantidade.value;
+    cpm2idade.value = cpm1idade.value;
+    cpm2peso.value = cpm1peso.value;
+    cpm2especie.radio = cpm1especie.radio
+}
+
+cpm1quantidade.addEventListener('input', atualizacpm2);
+cpm1idade.addEventListener('input', atualizacpm2);
+cpm1peso.addEventListener('input', atualizacpm2);
+cpm1especie.addEventListener('inut', atualizacpm2);
+
+function gerapdf(){
+    window.print();
+}
