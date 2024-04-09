bd40fd62b7127d416468b11472223bacaf69e371
Matheus Gregor <159480088+nuvemnm@users.noreply.github.com>
2024-04-09 08:25:59 -0300

    Add files via upload

--- site/animal.js
+++ site/animal.js
@@ -3,27 +3,67 @@
 const cpm1quantidade = document.querySelector('#quantidade');
 const cpm1idade = document.querySelector('#idade');
 const cpm1peso = document.querySelector('#peso');
-const cpm1especie = document.querySelector('#especie');
-
+const cpm1rato = document.querySelector('#rato');
+const cpm1camundongo = document.querySelector('#camundongo');
+const cpm1femea = document.querySelector('#femea');
+const cpm1macho = document.querySelector('#macho');
+const cpm1swiss= document.querySelector('#swiss');
+const cpm1balbc = document.querySelector('#balbc');
+const cpm1c57bl6j = document.querySelector('#c57bl6j');
 
 
 const cpm2quantidade = document.querySelector('#quantidade2');
 const cpm2idade = document.querySelector('#idade2');
 const cpm2peso = document.querySelector('#peso2');
-const cpm2especie = document.querySelector('#especie')
+const cpm2rato = document.querySelector('#rato2');
+const cpm2camundongo = document.querySelector('#camundongo2');
+const cpm2femea = document.querySelector('#femea2');
+const cpm2macho = document.querySelector('#macho2');
+const cpm2swiss= document.querySelector('#swiss2');
+const cpm2balbc = document.querySelector('#balbc2');
+const cpm2c57bl6j = document.querySelector('#c57bl6j2');
+
 
 function atualizacpm2(){
     cpm2quantidade.value = cpm1quantidade.value;
     cpm2idade.value = cpm1idade.value;
     cpm2peso.value = cpm1peso.value;
-    cpm2especie.radio = cpm1especie.radio
 }
 
-cpm1quantidade.addEventListener('input', atualizacpm2);
-cpm1idade.addEventListener('input', atualizacpm2);
-cpm1peso.addEventListener('input', atualizacpm2);
-cpm1especie.addEventListener('inut', atualizacpm2);
+function atualizaespecie(){
+    if(cpm1rato.checked){
+        cpm2rato.checked = true;
+    }else if(cpm1camundongo.checked){
+        cpm2camundongo.checked = true;
+    }
+}
 
+function atualizasexo(){
+    if(cpm1femea.checked){
+        cpm2femea.checked = true;
+    }else if(cpm1macho.checked){
+        cpm2macho.checked = true;
+    }
+}
+
+function atualizalinhagem(){
+    if(cpm1swiss.checked){
+        cpm2swiss.checked = true;
+    }else if(cpm1balbc.checked){
+        cpm2balbc.checked = true;
+    }else if(cpm1c57bl6j.checked){
+        cpm2c57bl6j.checked = true;
+    }
+}
+
+//chama funções detro do container
+container.addEventListener('input', atualizacpm2);
+container.addEventListener('change', atualizaespecie);
+container.addEventListener('change', atualizasexo);
+container.addEventListener('change', atualizalinhagem);
+
+
+//função print
 function gerapdf(){
     window.print();
 }
